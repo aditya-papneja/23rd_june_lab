111bda376e8ef39b73a3e6b2ae2ecb7da9f83dbd
Aditya Papneja <apapneja@confederationcollege.ca>
2021-08-12 22:19:32 -0500

    23rd june lab

---
+++ ContosoPets.Ui/Pages/Products/Create.cshtml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoPets.Ui.Models;
+using ContosoPets.Ui.Services;
+
+namespace ContosoPets.Ui.Pages.Products
+{
+    public class CreateModel : PageModel
+    {
+       private readonly ProductService _productService;
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public CreateModel(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _productService.CreateProduct(Product);
+
+            return RedirectToPage("Index");
+        }
+    }
+}
